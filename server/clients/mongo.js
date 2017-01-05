644dd9f2a38f43b2b19d34f3009bfcd0bb02141e
Darren Hurley <ironsidevsquincy@googlemail.com>
2017-01-05 14:46:10 +0000

    Use env var for mongodb uri

--- server/clients/mongo.js
+++ server/clients/mongo.js
@@ -1,6 +1,7 @@
 const denodeify = require('denodeify');
 const MongoClient = require('mongodb').MongoClient;
 
-const mongoClient = denodeify(MongoClient.connect)('mongodb://localhost:27017/next-video-editor');
+const mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/next-video-editor'
+const mongoClient = denodeify(MongoClient.connect)(mongodbUri);
 
 module.exports = mongoClient;
