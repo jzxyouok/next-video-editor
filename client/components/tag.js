2dd3bbcd5c1080ea081c2ba58b69aab64809ab99
Darren Hurley <ironsidevsquincy@googlemail.com>
2017-01-05 14:40:43 +0000

    Linting

--- client/components/tag.js
+++ client/components/tag.js
@@ -1,10 +1,9 @@
 const tag = () => {
 	[...document.querySelectorAll('.js-tag')]
 		.forEach(tag => {
-			tag.addEventListener('keyup', function (ev) {
+			tag.addEventListener('keyup', function () {
 				fetch(`https://tag-facets-api.ft.com/?partial=${this.value}&minimum=0&count=5`)
-					.then(response => {
-						console.log(repsponse);
+					.then(() => {
 					});
 			});
 		});
